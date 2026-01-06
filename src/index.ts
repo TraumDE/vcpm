57f7736853ce9dc63b0bf00b60979f202fa0cc93
TraumDE <eugene301workmaik@gmail.com>
2026-01-06 14:14:28 +0300

    Create base commander program

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
 #!/usr/bin/env node
+import { Command } from "commander";
 
-console.log("Test");
+const program: Command = new Command();
+
+program
+  .name("vcpm")
+  .description(
+    "VCPM - Voxel Core Project Manager. Is unoficall CLI util for managing content packs"
+  )
+  .version("0.1.0");
+
+program.parse();
