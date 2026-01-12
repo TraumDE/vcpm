dbb054dfbd895534dbdb181bbb50a2ae01b79a0d
TraumDE <eugene301workmaik@gmail.com>
2026-01-12 20:36:07 +0300

    Add test autocompletion

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,18 @@ program
   .description(
     "VCPM - Voxel Core Project Manager. Is unoficall CLI util for managing content packs",
   )
-  .version("0.1.0");
+  .version("0.3.0")
+  .enableCompletion();
 
 program
   .command("build")
   .description("Build project for production")
   .option("-d, --dev", "Build project for development", false)
+  .completion({
+    positionalany: ["$files"],
+  })
   .action(build);
+
+await program.installCompletion();
 
 program.parse();
