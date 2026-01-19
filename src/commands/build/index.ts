d5aad7fc8c9e9d01d3e03b9c8c1f135737c0b7cf
TraumDE <eugene301workmaik@gmail.com>
2026-01-20 02:56:33 +0300

    End of rewrite project

--- src/commands/build/index.ts
+++ src/commands/build/index.ts
@@ -3,12 +3,13 @@ import {BlobReader, BlobWriter, ZipWriter} from '@zip.js/zip.js'
 import {glob, Path} from 'glob'
 import {Buffer} from 'node:buffer'
 import {promises as fs} from 'node:fs'
-import {join} from 'node:path'
+
+import type {PackageInfo} from '../../types/index.js'
 
 export class Build extends Command {
   static description = 'build project'
   static flags = {
-    dev: Flags.boolean({char: 'd'}),
+    dev: Flags.boolean({char: 'd', description: 'build in development mode'}),
   }
 
   public async run(): Promise<void> {
@@ -17,29 +18,50 @@ export class Build extends Command {
   }
 
   private async addFile(file: Path, zipWriter: ZipWriter<Blob>): Promise<void> {
-    const blobReader: BlobReader = new BlobReader(new Blob([new Uint8Array(await fs.readFile(file.relative()))]))
-    await zipWriter.add(file.relative(), blobReader)
+    const relativePath: string = file.relative()
+    await zipWriter.add(relativePath, new BlobReader(new Blob([new Uint8Array(await fs.readFile(relativePath))])))
   }
 
   private async build(): Promise<void> {
+    try {
+      await fs.access('dist')
+    } catch {
+      await fs.mkdir('dist')
+    }
+
     const zipFileWriter: BlobWriter = new BlobWriter('application/zip')
     const zipWriter: ZipWriter<Blob> = new ZipWriter(zipFileWriter)
+    const {flags} = await this.parse(Build)
+
     const files: Path[] = await glob('**/*', {
       cwd: process.cwd(),
-      ignore: ['node_modules/**/*', 'dist/**/*'],
+      dot: true,
+      ignore: flags.dev ? [] : ['**/.*', '**/.*/**', 'dist/**/*', 'modules/types/**/*'],
       nodir: true,
-
       withFileTypes: true,
     })
     const filePromises: Promise<void>[] = files.map((file) => this.addFile(file, zipWriter))
-
     await Promise.all(filePromises)
 
     const zipBlob: Blob = await zipWriter.close()
     const buffer: Buffer = Buffer.from(await zipBlob.arrayBuffer())
 
-    await fs.writeFile('Archive.zip', buffer)
+    const packageInfo: PackageInfo = await this.readPackage()
+    await fs.writeFile(`dist/${packageInfo.id}_${packageInfo.version}.zip`, buffer)
 
-    this.log('Producion build completed!')
+    if (flags.dev) {
+      this.log('Development build completed!')
+    } else {
+      this.log('Production build completed!')
+    }
+  }
+
+  private async readPackage(): Promise<PackageInfo> {
+    const packageJsonParsed = JSON.parse(await fs.readFile('package.json', 'utf8'))
+
+    return {
+      id: packageJsonParsed.id,
+      version: packageJsonParsed.version,
+    }
   }
 }
