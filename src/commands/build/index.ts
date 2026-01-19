ecd210d249c9bf30697917e7a46d71d56a3700db
TraumDE <eugene301workmaik@gmail.com>
2026-01-20 01:12:47 +0300

    First work version

--- src/commands/build/index.ts
+++ src/commands/build/index.ts
@@ -1,5 +1,6 @@
 import {Command, Flags} from '@oclif/core'
 import {BlobReader, BlobWriter, ZipWriter} from '@zip.js/zip.js'
+import {glob, Path} from 'glob'
 import {Buffer} from 'node:buffer'
 import {promises as fs} from 'node:fs'
 import {join} from 'node:path'
@@ -15,17 +16,28 @@ export class Build extends Command {
     await this.build()
   }
 
+  private async addFile(file: Path, zipWriter: ZipWriter<Blob>): Promise<void> {
+    const blobReader: BlobReader = new BlobReader(new Blob([new Uint8Array(await fs.readFile(file.relative()))]))
+    await zipWriter.add(file.relative(), blobReader)
+  }
+
   private async build(): Promise<void> {
     const zipFileWriter: BlobWriter = new BlobWriter('application/zip')
     const zipWriter: ZipWriter<Blob> = new ZipWriter(zipFileWriter)
+    const files: Path[] = await glob('**/*', {
+      cwd: process.cwd(),
+      ignore: ['node_modules/**/*', 'dist/**/*'],
+      nodir: true,
 
-    const test: Buffer = await fs.readFile(join(process.cwd(), 'LICENSE'))
-    const blobs: Blob = new Blob([new Uint8Array(test)])
-    const blobReader: BlobReader = new BlobReader(blobs)
-    await zipWriter.add('LICENSE', blobReader)
+      withFileTypes: true,
+    })
+    const filePromises: Promise<void>[] = files.map((file) => this.addFile(file, zipWriter))
+
+    await Promise.all(filePromises)
 
     const zipBlob: Blob = await zipWriter.close()
     const buffer: Buffer = Buffer.from(await zipBlob.arrayBuffer())
+
     await fs.writeFile('Archive.zip', buffer)
 
     this.log('Producion build completed!')
